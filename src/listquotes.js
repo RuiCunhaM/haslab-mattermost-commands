f1e89f96838f6a518eb5f322002f9910e70034cc
rui <ruicunham@hotmail.com>
2025-01-31 16:02:57 +0000

    fix(listquotes): handle empty results

--- src/listquotes.js
+++ src/listquotes.js
@@ -9,9 +9,7 @@ export async function listquotes(env, body) {
 	if (author === null || author.length === 0) quotes = await getAllQuotes(env);
 	else quotes = await getAllQuotesFromAuthor(env, author);
 
-	if (quotes.success) {
-		return quoteslist(quotes.results);
-	}
+	if (!quotes.success || quotes.results.length == 0) return quoteNotFound();
 
-	return quoteNotFound();
+	return quoteslist(quotes.results);
 }
